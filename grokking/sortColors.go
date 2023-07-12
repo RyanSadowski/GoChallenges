bc12799f956fbfad0c118f6ada5cf1b3420bb38e
RyanSadowski <sadowskiryan@gmail.com>
2023-07-11 21:50:38 -0700

    adding the preferred 3 pointer sort for 3 colors

--- grokking/sortColors.go
+++ grokking/sortColors.go
@@ -1,21 +1,49 @@
 package main
 
-
 import(
-"fmt"
+    "fmt"
 )
+
 func sortColors(colors []int) []int {
 
-	// Write your code here
+    return threePointer(colors)
+}
 
-	return selectionSort(colors)
+
+
+func threePointer(input []int) []int {
+    var red int = 0
+    var white int = 0
+    var blue = len(input) -1
+
+    for white <= blue {
+
+        if input[white] == 0 {
+            if input[red] != 0 {
+                input[red], input[white] = input[white], input[red]
+            }
+
+            white ++
+            red ++
+        } else if input[white] == 1 {
+            white ++
+        } else {
+            if input[blue] != 2 {
+                input[white], input[blue] = input[blue], input[white]
+            }
+
+            blue--
+        }
+    }
+
+    return input
 }
 
 
 func selectionSort(input []int) []int {
     var size int = len(input)
     for i := 0; i < size -1; i ++ {
-    
+
         for j := i + 1; j < size; j ++ {
             if input[j] < input[i]{
                 //swap
@@ -32,16 +60,16 @@ func bubbleSort(input []int) []int{
     var size = len(input)
     for i := 0; i < size -1; i ++ {
         for j := 0; j < size -1 - i; j ++ {
-               //swap
+            //swap
 
-               if(input[j] > input[j+1]) {
-                   input[j], input[j+1] = input[j+1], input[j]
-               }
+            if(input[j] > input[j+1]) {
+                input[j], input[j+1] = input[j+1], input[j]
+            }
         }
     }
 
     return input
-    
+
 }
 
 func main() {
